53688267b928730535acbd410a9c931b053b22a7
Gustavo Amezcua <GustavoA@innovision.local>
2021-10-28 16:09:09 -0700

    left at First Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 
 
 import { BiArchive } from 'react-icons/bi'
+import Search from './components/Search'
 
 function App() {
   return (
-    <div className="App">
-      <h1><BiArchive/>Your Appointments</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
+        <BiArchive className="inline-block text-red-400" />Your Appointments
+      </h1>
+      <Search/>
     </div>
   );
 }
